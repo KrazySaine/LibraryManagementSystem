23445609c0a34ac17f13b23442ba42a10b378eb6
christopher cane <christopher.cane@student.centralia.edu>
2025-01-17 18:34:02 -0800

    Add project files.

---
+++ LibraryManagementSystem/Controllers/LibraryController.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult displayBooks()
+        {
+            var model = new List< Book>();
+            model.Add(new Book { Title = "" });
+            model.Add(new Book { Author = "" });
+            return View(model);
+
+        }
+    }
+}
